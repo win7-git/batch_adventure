41de1c4aab3b35a94826cff7e41b5ded474ac62d
win7-git <spam.me.here@live.com>
2013-03-17 17:32:04 +0800

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,2 +1,9 @@
 cls
 @echo off
+if not exist userdata.dat (goto error0) else (goto verify)
+:verify
+echo User data file detected.
+echo Please enter password to unlock user data file.
+echo If a password does not exist, simply press Enter.
+set pass=
+set /p pass=
